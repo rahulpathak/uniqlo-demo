8cef3ba11e0c19cefa58b3926f2c75e74afc0a89
Venkatesh Periyasamy <vperiyasamy@altimetrik.com>
2019-07-22 17:28:39 +0530

    DASHBOARD PAGE FILTER ACTION COMPLIETED

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { XlsxDataService } from '../rootServices/xlsx-data.service';
 import { ProgressSuccessFailureComponent } from './progress-success-failure/progress-success-failure.component';
+import { VoiceStoreTableComponent } from './voice-store-table/voice-store-table.component';
 
 @NgModule({
-  declarations: [ProgressSuccessFailureComponent],
+  declarations: [ProgressSuccessFailureComponent, VoiceStoreTableComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    ProgressSuccessFailureComponent
+    ProgressSuccessFailureComponent, VoiceStoreTableComponent
   ],
   providers: [
     XlsxDataService
