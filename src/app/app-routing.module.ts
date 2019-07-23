28a65a8146298c2e768d99a57e9a7e21dd651c32
rahulpathak2410 <rpathak@altimetrik.com>
2019-07-23 15:31:20 +0530

    UI changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { VoiceofstoreComponent } from './voiceofstore/voiceofstore.component';
 import { VoiceofcustomerComponent } from './voiceofcustomer/voiceofcustomer.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'vos', component: VoiceofstoreComponent },
-  { path: 'voc', component: VoiceofcustomerComponent }
+  { path: 'voc', component: VoiceofcustomerComponent },
+  { path: 'productDetails', component: ProductDetailsComponent}
 ];
 
 @NgModule({
