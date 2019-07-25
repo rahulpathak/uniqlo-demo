c4ef6052b0d1d37034d71c506676f7da8ff0225b
Venkatesh Periyasamy <vperiyasamy@altimetrik.com>
2019-07-25 17:33:32 +0530

    Fix country accordion checkbox

--- src/app/product-view/product-country-stats/product-country-stats.component.ts
+++ src/app/product-view/product-country-stats/product-country-stats.component.ts
@@ -12,137 +12,9 @@ export class ProductCountryStatsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.country_stats =  [
-
-      {
-  
-        "country": "Japan",
-        'code': 'jp',
-  
-        "voc": [
-  
-          {
-  
-            "comment": "I would like to have a pocket",
-  
-            "count": 28
-  
-          },
-  
-          {
-  
-            "comment": "For office use, it will be helpful to have a pocket",
-  
-            "count": 48
-  
-          },
-  
-          {
-  
-            "comment": "A pocket will help me keep my pen and pencil",
-  
-            "count": 20
-  
-          }
-  
-        ],
-  
-        "vos": [
-  
-          {
-  
-            "comment": "Comfortable, easy to wear and easy to wash. Love it",
-  
-            "count": 28
-  
-          },
-  
-          {
-  
-            "comment": "Collar could have been better",
-  
-            "count": 48
-  
-          }
-  
-        ],
-  
-        "market_information": {
-  
-          "positive": 67,
-  
-          "negative": 33
-  
-        }
-  
-      }, 
-
-      {
-  
-        "country": "USA",
-        "code": "us",
-  
-        "voc": [
-  
-          {
-  
-            "comment": "I would like to have a pocket",
-  
-            "count": 28
-  
-          },
-  
-          {
-  
-            "comment": "For office use, it will be helpful to have a pocket",
-  
-            "count": 48
-  
-          },
-  
-          {
-  
-            "comment": "A pocket will help me keep my pen and pencil",
-  
-            "count": 20
-  
-          }
-  
-        ],
-  
-        "vos": [
-  
-          {
-  
-            "comment": "Comfortable, easy to wear and easy to wash. Love it",
-  
-            "count": 28
-  
-          },
-  
-          {
-  
-            "comment": "Collar could have been better",
-  
-            "count": 48
-  
-          }
-  
-        ],
-  
-        "market_information": {
-  
-          "positive": 67,
-  
-          "negative": 33
-  
-        }
-  
-      }
-  
-    ];
-
-    this.country_stats.map(x => {x.checked = true; return x;})
+    if(this.product && this.product.details && this.product.details.comments) {
+      this.product.details.comments.map(x => {x.checked = true; return x;})
+    }
   }
 
   getCountryImageName(country) {
@@ -152,7 +24,6 @@ export class ProductCountryStatsComponent implements OnInit {
   clickCountrySelection(stats, event) {
     event.stopPropagation();
     stats.checked = !stats.checked;
-    console.log(this.country_stats);
   }
 
 }
