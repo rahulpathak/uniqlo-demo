5bec199f17eaca6d889077c707ac88bee6dab9fe
Venkatesh Periyasamy <vperiyasamy@altimetrik.com>
2019-07-26 13:00:44 +0530

    Collapse / Expand All for country accordian

--- src/app/product-view/product-country-stats/product-country-stats.component.ts
+++ src/app/product-view/product-country-stats/product-country-stats.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 })
 export class ProductCountryStatsComponent implements OnInit {
   @Input('product') product;
-  country_stats;
+  openAll: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.openAll = true;
     if(this.product && this.product.details && this.product.details.comments) {
-      this.product.details.comments.map(x => {x.checked = true; return x;})
+      this.product.details.comments.map(x => {
+        x.opened = true;
+        x.checked = true;
+        return x;
+      })
     }
   }
 
@@ -26,4 +32,19 @@ export class ProductCountryStatsComponent implements OnInit {
     stats.checked = !stats.checked;
   }
 
+  toggleAll() {
+    this.openAll = !this.openAll;
+    if(this.product && this.product.details && this.product.details.comments) {
+      this.product.details.comments.map(x => {
+        x.opened = this.openAll;
+        return x;
+      })
+    }
+  }
+
+  onOpenChangePanel(data, stats) {
+    stats.opened = data;
+    this.openAll = false;
+    this.product.details.comments.forEach(x => x.opened ? this.openAll = true : '');
+  }
 }
