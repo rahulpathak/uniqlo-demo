0a16c2ad26a2ef7b4b41086341876297d4954f03
Venkatesh Periyasamy <vperiyasamy@altimetrik.com>
2019-08-02 16:00:18 +0530

    Filter done

--- src/app/layout/components/filter-list/filter-list.component.ts
+++ src/app/layout/components/filter-list/filter-list.component.ts
@@ -29,6 +29,29 @@ export class FilterListComponent implements OnInit {
     this.xls.getFilterJson().subscribe(filterJson => this.filterJson = filterJson)
   }
 
+  toogleFilter(key, filter?) {
+    if(!this.filterJson[key]) {
+      this.filterJson[key] = [];
+    }
+    if(this.checkFilterExists(key, filter)) {
+      this.filterJson[key] = this.filterJson[key].filter(x => x.toLowerCase() != filter.toLowerCase());
+    } else if(filter) {
+      this.filterJson[key].push(filter);
+    }
+    this.xls.triggerFilter(this.filterJson);
+  }
+
+  checkFilterExists(key, filter?) {
+    let filterKeyExists = this.filterJson && this.filterJson[key] && this.filterJson[key].length > 0;
+    if(filterKeyExists) {
+      if(filter) {
+        return this.filterJson[key].some(x => x.toLowerCase() === filter.toLowerCase());
+      }
+      return true;
+    }
+    return false;
+  }
+
   isFilterEmpty() {
     if(!this.filterJson || typeof this.filterJson != 'object') return false;
     for(let key in this.filterJson) {
